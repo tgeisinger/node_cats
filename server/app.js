dce2cca3e2fe1d09b2b760c8b8fe126bcd8c204a
Thomas Geisinger <thomasgeisinger@Thomass-MacBook-Pro.local>
2016-05-13 14:07:01 -0500

    intial commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+// Node Server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+
+app.set('port', 5000);
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.use('/', index);
+
+
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Server is ready on port:' + app.get('port'));
+});
