eb17e21d7f960bf1dcd89ef47aa8b92efe59b135
Thomas Geisinger <thomasgeisinger@Thomass-MacBook-Pro.local>
2016-05-13 15:59:11 -0500

    port change

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ var app = express();
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
-app.set('port', 5000);
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+app.use('/felines', module);
 
 app.use('/', index);
 
